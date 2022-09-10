5a82016808c8b668392fea8eb7f71b404a118485
Anton G. Neuhold Jr <agneuhold@gmail.com>
2022-09-10 15:39:28 -0700

    Add basic mongodb connection

--- server/src/config.ts
+++ server/src/config.ts
@@ -8,6 +8,8 @@ dotenv.config();
 const config = {
   mongoRootUsername: process.env.MONGO_ROOT_USERNAME,
   mongoRootPassword: process.env.MONGO_ROOT_PASSWORD,
+  mongoPort: process.env.MONGO_PORT,
+  mongoUrl: process.env.MONGO_URL,
 };
 
 export default config;
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,8 @@
+import mongoose from 'mongoose';
 import config from './config';
 
 console.log('The server is beginning to start...');
 
-const mongoDbConnectionString = `mongodb://${config.mongoRootUsername}:${config.mongoRootPassword}`;
-console.log(mongoDbConnectionString);
+const mongoDbConnectionString = `mongodb://${config.mongoRootUsername}:${config.mongoRootPassword}@${config.mongoUrl}:${config.mongoPort}`;
+const conn = mongoose.createConnection(mongoDbConnectionString);
+console.log(conn);
