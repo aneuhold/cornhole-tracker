1284ee845d6016527e860e914815ce530094f52e
Anton G. Neuhold Jr <agneuhold@gmail.com>
2022-09-16 20:38:25 -0700

    Add basic express setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,16 @@
+import express, { Express } from 'express';
+import http from 'http';
+import https from 'https';
 import 'module-alias/register';
-import User from 'shared/types/User';
-import UserRepository from './repositories/UserRepository';
 import DocumentDb from './util/DocumentDb';
+
+const NODE_SERVER_PORT = 3020;
 
 async function main() {
   console.log('The server is beginning to start...');
-  const newUser = new User('someTestUser');
-  const result = await UserRepository.insertNewUser(newUser);
-  console.log(result);
+  const app = express();
+  setupExpressRoutes(app);
+  createServer(app, NODE_SERVER_PORT);
 }
 
 main()
@@ -16,3 +19,23 @@ main()
   .finally(() => {
     DocumentDb.closeDbConnection();
   });
+
+function setupExpressRoutes(app: Express) {
+  app.get('/ping', async (_req, res) => {
+    res.send({
+      message: 'pong'
+    });
+  });
+}
+
+function createServer(app: Express, port: number, isHttps = false) {
+  if (isHttps) {
+    https.createServer(app).listen(port, () => {
+      console.log(`HTTP server listening on port ${port}`);
+    });
+  } else {
+    http.createServer(app).listen(port, () => {
+      console.log(`HTTPS server listening on port ${port}`);
+    });
+  }
+}
