d046887e21a780085a6a2d9b1a83b7d99d30dc33
Anton G Neuhold Jr <agneuhold@gmail.com>
2022-09-26 05:09:46 -0700

    Fix up vite config

--- client/src/routes/componentlibrary/+page.svelte
+++ client/src/routes/componentlibrary/+page.svelte
@@ -1,6 +1,11 @@
+<script lang="ts">
+	import SideBar from '$lib/SideBar.svelte';
+</script>
+
 <svelte:head>
 	<title>Component Library</title>
 	<meta name="description" content="The component library for this app" />
 </svelte:head>
 
 <p>This is the component library</p>
+<SideBar links={['test']} />
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,22 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import path from 'path';
 import type { UserConfig } from 'vite';
 
 /** @type {import('vite').UserConfig} */
 const config: UserConfig = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	resolve: {
+		alias: {
+			src: path.resolve('./src'),
+			shared: path.resolve('./_shared'),
+			$lib: path.resolve('./src/lib')
+		}
+	},
+	server: {
+		fs: {
+			allow: ['./_shared', './src']
+		}
+	}
 };
 
 export default config;
