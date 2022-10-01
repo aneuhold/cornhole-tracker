a1b56966ea5ede2b82882c8fbf92e9a1d674a0d9
Anton G Neuhold Jr <agneuhold@gmail.com>
2022-10-01 14:08:02 -0500

    Add tooltip component and more logic to table

--- client/src/lib/IconButton.svelte
+++ client/src/lib/IconButton.svelte
@@ -14,6 +14,13 @@
 				iconHtml: svgIcons.twitter
 			},
 			listeners
+		},
+		'Non Primary': {
+			props: {
+				iconHtml: svgIcons.twitter,
+				primary: false
+			},
+			listeners
 		}
 	};
 </script>
@@ -23,7 +30,20 @@
 
 	export let iconHtml: string;
 
+	export let primary = true;
+
+	export let raised = true;
+
 	const dispatch = createEventDispatcher();
+
+	const standardIconButtonClasses =
+		'inline-block p-3 font-medium text-xs leading-tight uppercase rounded-full focus:outline-none focus:ring-0  active:shadow-lg transition duration-150 ease-in-out mx-1';
+
+	const raisedClasses = raised ? 'shadow-md hover:shadow-lg focus:shadow-lg' : '';
+
+	const colorClasses = primary
+		? 'active:bg-blue-800 fill-white bg-blue-500 focus:bg-blue-700 hover:bg-blue-700'
+		: 'active:bg-gray-800 border-blue-500 fill-blue-500 bg-white hover:border-transparent focus:bg-gray-300 hover:bg-gray-300';
 
 	/**
 	 * Optional click handler
@@ -35,7 +55,7 @@
 
 <button
 	type="button"
-	class="inline-block p-3 bg-blue-500 fill-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out mx-1"
+	class={standardIconButtonClasses + ' ' + colorClasses + ' ' + raisedClasses}
 	on:click={onClick}
 >
 	{@html iconHtml}
--- client/src/lib/Table.svelte
+++ client/src/lib/Table.svelte
@@ -1,5 +1,6 @@
 <script lang="ts" context="module">
 	import type { ComponentStories } from 'src/routes/componentlibrary/+page.svelte';
+	import svgIcons from 'src/util/svgIcons';
 
 	export type TableData = {
 		headers: string[];
@@ -10,7 +11,7 @@
 		'With Small Amount of Data': {
 			props: {
 				tableData: {
-					headers: ['test header 1', 'test header 2', 'test header 3'],
+					headers: ['Test header 1', 'Test header 2', 'Test header 3'],
 					rows: [
 						['data 1', 'data 2', 'data 3'],
 						['data 1', 'data 2', 'data 3'],
@@ -25,10 +26,40 @@
 		'With Title': {
 			props: {
 				tableTitle: 'Some Test Title',
-				tableTitleClasses: 'bg-blue-100',
+				tableClasses: 'bg-blue-100',
 				tableData: {
 					headers: ['test header 1', 'test header 2', 'test header 3'],
 					rows: [
+						['data 1', 'data 2', 'data 3'],
+						['data 1', 'data 2', 'data 3'],
+						['data 1', 'data 2', 'data 3'],
+						['data 1', 'data 2', 'data 3'],
+						['data 1', 'data 2', 'data 3']
+					]
+				}
+			}
+		},
+		'With a lot of data': {
+			props: {
+				tableTitle: 'Some Test Title',
+				tableClasses: 'bg-green-100',
+				tableDescription: 'This is a test table with a lot of data',
+				tableData: {
+					headers: ['test header 1', 'test header 2', 'test header 3'],
+					rows: [
+						['data 1', 'data 2', 'data 3'],
+						['data 1', 'data 2', 'data 3'],
+						['data 1', 'data 2', 'data 3'],
+						['data 1', 'data 2', 'data 3'],
+						['data 1', 'data 2', 'data 3'],
+						['data 1', 'data 2', 'data 3'],
+						['data 1', 'data 2', 'data 3'],
+						['data 1', 'data 2', 'data 3'],
+						['data 1', 'data 2', 'data 3'],
+						['data 1', 'data 2', 'data 3'],
+						['data 1', 'data 2', 'data 3'],
+						['data 1', 'data 2', 'data 3'],
+						['data 1', 'data 2', 'data 3'],
 						['data 1', 'data 2', 'data 3'],
 						['data 1', 'data 2', 'data 3'],
 						['data 1', 'data 2', 'data 3'],
@@ -43,6 +74,8 @@
 </script>
 
 <script lang="ts">
+	import IconButton from './IconButton.svelte';
+
 	export let tableData: TableData;
 
 	/**
@@ -51,37 +84,63 @@
 	export let tableTitle: string;
 
 	/**
+	 * The description for the table.
+	 */
+	export let tableDescription: string;
+
+	/**
 	 * Optional classes for the table title. This can be used to color them.
 	 */
-	export let tableTitleClasses: string;
+	export let tableClasses: string;
 </script>
 
-<div />
-{#if tableTitle}
-	<h3 class={tableTitleClasses + ' rounded-lg p-2 text-lg'}>{tableTitle}</h3>
-{/if}
-<table class="table-auto table centerText bg-slate-200 rounded-lg">
-	<thead>
-		<tr class="border-b border-black">
-			{#each tableData.headers as header}
-				<th>{header}</th>
-			{/each}
-		</tr>
-	</thead>
-	<tbody>
-		{#each tableData.rows as rowData}
-			<tr class="border-b border-black">
-				{#each rowData as rowValue}
-					<td class="px-6 py-4">{rowValue}</td>
+<div class={tableClasses + ' rounded-xl px-3 pb-3'}>
+	<div class="tableHeader">
+		{#if tableTitle}
+			<h3 class="rounded-lg p-2 text-lg mr-auto">{tableTitle}</h3>
+		{/if}
+		{#if tableDescription}
+			<div class="m-3">
+				<IconButton primary={false} raised={false} iconHtml={svgIcons.info} />
+			</div>
+		{/if}
+	</div>
+	<div class="scrollArea">
+		<table class="table-auto table centerText bg-white rounded-lg">
+			<thead>
+				<tr class="border-b border-black">
+					{#each tableData.headers as header}
+						<th>{header}</th>
+					{/each}
+				</tr>
+			</thead>
+			<tbody>
+				{#each tableData.rows as rowData}
+					<tr class="border-b border-black">
+						{#each rowData as rowValue}
+							<td class="px-6 py-4">{rowValue}</td>
+						{/each}
+					</tr>
 				{/each}
-			</tr>
-		{/each}
-	</tbody>
-</table>
+			</tbody>
+		</table>
+	</div>
+</div>
 
 <style>
 	.table {
 		width: 100%;
+	}
+	.tableHeader {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		gap: auto;
+	}
+	.scrollArea {
+		/* To make sure that the height doesn't get too large and it scrolls. */
+		max-height: 100vh;
+		overflow-y: scroll;
 	}
 	.centerText {
 		text-align: center;
