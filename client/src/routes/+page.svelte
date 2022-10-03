fad27998d9564e85f63120ead0beead770a34f70
Anton G. Neuhold Jr <agneuhold@gmail.com>
2022-10-02 17:10:48 -0700

    Add redirect to index page

---
+++ client/src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import { user } from 'src/stores/user';
+
+  user.subscribe((updatedUser) => {
+    if (!updatedUser.userName) {
+      window.location.href = '/login';
+    }
+  });
+</script>
